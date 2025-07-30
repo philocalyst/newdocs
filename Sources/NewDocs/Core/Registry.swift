17a1f5f17585a13c2873002a5f6146c5ca83d684
miles <regime_skylark.0y@icloud.com>
2025-07-30 02:13:45 -0400

    Switched to a Result<> structure
    Heard throwing was a different thing

--- Sources/NewDocs/Core/Registry.swift
+++ Sources/NewDocs/Core/Registry.swift
@@ -1,6 +1,6 @@
 public protocol PackageRegistry {
-  func search_packages(for query: String) async throws -> [Package]  // Search all of the available packages for a specific language and return any hits, where no hits are represented as an emtpy array (async and possible failures due to network requests)
-  func get_package(UUID: UInt64) async throws -> Package  // Find a specific package by UUID (can fail because it's performing a lookup, async for the same reason) A failed lookup will throw an error.
-  func get_package(named: String) async throws -> [Package]  // Find all packages that share a specific name
-  func get_reference() -> Package  // Return the package reflecting the language reference. Not falliable.
+  func search_packages(for query: String) async -> Result<[Package], NewDocsError>  // Search all of the available packages for a specific language and return any hits, where no hits are represented as an emtpy array (async and possible failures due to network requests)
+  func get_package(UUID: UInt64) async -> Result<Package, NewDocsError>  // Find a specific package by UUID (can fail because it's performing a lookup, async for the same reason) A failed lookup will throw an error.
+  func get_package(named: String) async -> Result<[Package], NewDocsError>  // Find all packages that share a specific name
+  func get_reference() async -> Package  // Return the package reflecting the language reference. Not falliable as it's just filling in known information largely.
 }
