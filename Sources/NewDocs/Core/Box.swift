61959e724409e5eafcedf26c377860bd90efe907
miles <regime_skylark.0y@icloud.com>
2025-07-30 04:07:33 -0400

    Switched to Int64 for hashing overflows

--- Sources/NewDocs/Core/Box.swift
+++ Sources/NewDocs/Core/Box.swift
@@ -2,8 +2,9 @@ import SemVer
 
 public protocol Package {
   var slug: String { get set }  // The true/unqualified name
+  var name: String { get set }  // The true/unqualified name
   var lang: Language { get set }  // The language it's related to
-  var UUID: UInt64 { get }  // The identifier of the package
+  var UUID: Int64 { get }  // The identifier of the package
   var source: String { get }  // The associated parent/host; like the package manager it's hosted upon, or just the fact that it's the standard library reference. For a crates.io package, it might be "crates"
 
   // These are all async due to possible network requests/file IO
@@ -14,7 +15,7 @@ public protocol Package {
   func dependents() async -> Result<[Package], NewDocsError>  // Any other packages that rely on this package (none is an empty array)
 
   // We should only require the slug for initialization. The name is purely cosmetic so not required
-  init(slug: String, name: String?)
+  init(slug: String, name: String?, description: String?)
 
   func retrieve(at version: Version, flags: [String]?) async throws -> Documentation  // With the information, get the most relevant, precise doc. It's up to the implementer to create default behavior if applicable for the version specifier. Not every language package has the concept of flags so it can just be passed in as nil. Can throw as this is a signficant resolution step. A lot could go wrong.
 }
