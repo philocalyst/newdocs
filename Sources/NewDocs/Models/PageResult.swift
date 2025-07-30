3b6eaa6f8e905a077bc1755b9da63fd09707f03a
miles <regime_skylark.0y@icloud.com>
2025-07-30 00:49:53 -0400

    Simplified the page result structure

--- Sources/NewDocs/Models/PageResult.swift
+++ Sources/NewDocs/Models/PageResult.swift
@@ -2,22 +2,16 @@ import Foundation
 
 public struct PageResult {
   public let path: String
-  public let storePath: String
-  public let output: String
+  public let content: String
   public let entries: [Entry]
-  public let internalURLs: [String]
 
   public init(
     path: String,
-    storePath: String,
     output: String,
     entries: [Entry],
-    internalURLs: [String] = []
   ) {
     self.path = path
-    self.storePath = storePath
-    self.output = output
+    self.content = output
     self.entries = entries
-    self.internalURLs = internalURLs
   }
 }
