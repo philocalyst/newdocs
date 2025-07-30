a874a00ad1696cbc93b91c8ec203f0ece5726767
miles <regime_skylark.0y@icloud.com>
2025-07-30 02:14:04 -0400

    Added back internal urls

--- Sources/NewDocs/Models/PageResult.swift
+++ Sources/NewDocs/Models/PageResult.swift
@@ -3,15 +3,18 @@ import Foundation
 public struct DocumentationPage {
   public let path: String
   public let content: String
+  public let internalURLs: [URL]
   public let entries: [Entry]
 
   public init(
     path: String,
-    output: String,
+    content: String,
+    internalURLs: [URL],
     entries: [Entry],
   ) {
     self.path = path
-    self.content = output
+    self.content = content
     self.entries = entries
+    self.internalURLs = internalURLs
   }
 }
