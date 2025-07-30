0b1e91eebd0d9c1cfa61dc26e2c7a28d9cb36a35
miles <regime_skylark.0y@icloud.com>
2025-07-30 00:47:51 -0400

    Added the SemVer package

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
     .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.8.0"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+    .package(url: "https://github.com/sersoft-gmbh/semver", from: "5.0.0"),
   ],
   targets: [
     .target(
@@ -21,6 +22,7 @@ let package = Package(
       dependencies: [
         "SwiftSoup",
         "Alamofire",
+        .product(name: "SemVer", package: "semver"),
         .product(name: "Logging", package: "swift-log"),
       ]
     )
